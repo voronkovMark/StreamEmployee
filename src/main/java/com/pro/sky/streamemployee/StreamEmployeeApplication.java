39b76d2062dc040c3ec03dfbc8d889cf6dd19c73
Mark <voronkov1mark@gmail.com>
2023-05-31 23:37:45 +0300

    initial commit

---
+++ src/main/java/com/pro/sky/streamemployee/StreamEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.streamemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pro/sky/streamemployee/StreamEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.streamemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
